b36c7828c19c1dffcae8a69c3106d8d3024951b7
xuezhongde <xuezhongde@gmail.com>
2019-11-25 20:44:08 +0800

    .

--- src/main/java/org/zdxue/zk/console/common/GlobalControllerExceptionHandler.java
+++ src/main/java/org/zdxue/zk/console/common/GlobalControllerExceptionHandler.java
@@ -2,14 +2,23 @@ package org.zdxue.zk.console.common;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
  * @author xuezhongde
  */
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalControllerExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
+    @ExceptionHandler(value = {NoHandlerFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String noHandlerFoundException(Exception ex) {
+        return "404";
+    }
 
 }
